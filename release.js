befeb09d2e5ca86e7084d72dba3de5e61fe20f2a
Mark Voorberg <mark@binaryops.ca>
2018-06-20 15:33:19 -0700

    updates to release.js

--- release.js
+++ release.js
@@ -1,29 +1,48 @@
 const shell = require('shelljs');
 
-// Is the repos all checked-in?
-const isClean = exec('git status --porcelain');
-if (isClean.stdout || isClean.stderr) {
-    console.error('Git working directory is not clean.');
-    console.error(isClean.stdout, isClean.stderr);
-    process.exit(2);
-}
+// // Is the repos all checked-in?
+// const isClean = exec('git status --porcelain');
+// if (isClean.stdout || isClean.stderr) {
+//     console.error('Git working directory is not clean.');
+//     console.error(isClean.stdout, isClean.stderr);
+//     process.exit(2);
+// }
 
 // Get major/minor/patch from the command line
-const versionIncrement = process.argv[process.argv.length - 1];
-
-if (versionIncrement != 'major' && versionIncrement != 'minor' && versionIncrement != 'patch') {
-    console.error('Usage: node release.js major|minor|patch');
+let versionIncrement;
+if (process.argv.length > 2) {
+    versionIncrement = process.argv[2];
+}
+const validVersions = ['major', 'minor', 'patch'];
+if (validVersions.indexOf(versionIncrement) === -1) {
+    console.error(`Usage: node release.js ${validVersions.join('|')} [beta]`);
     process.exit(1);
 }
 
-// Increment the version in package.json
-exec('npm version ' + versionIncrement);
+let isBeta = false;
+if (process.argv.length > 3) {
+    isBeta = (process.argv[3] === 'beta');
+}
 
-// Run tests and deploy to npm.
-exec('npm test');
-exec('git push');
-exec('git push --tags');
-exec('npm publish --access public');
+if (isBeta) {
+    // Increment the version in package.json & create a new tag in git.
+    exec(`npm version ${versionIncrement} -m "Beta version, ${versionIncrement}"`);
+
+    // Run tests and deploy to npm.
+    exec('npm test');
+    exec('git push');
+    exec('git push --tags');
+    exec('npm publish --access public --tag beta');
+} else {
+    // Increment the version in package.json & create a new tag in git.
+    exec(`npm version ${versionIncrement}`);
+
+    // Run tests and deploy to npm.
+    exec('npm test');
+    exec('git push');
+    exec('git push --tags');
+    exec('npm publish --access public');
+}
 
 // Run commands quietly, log output and exit only on errors.
 function exec(cmd) {
